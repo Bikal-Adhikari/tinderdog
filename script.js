2a1251b5cfc6fc715f9cfc960eeea32055df6feb
Bikal <bikaladhikari5@gmail.com>
2024-03-21 18:58:46 +1100

    Script to fetch data

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 let dogList = [];
-const api = "https://dogapi.dog/api-docs/v2/swagger.json";
+const api = "https://dogapi.dog/api/v2/breeds";
+const dogListELm = document.getElementById("list");
 
-const fetchUsers = async (url = api) => {
+const fetchDogs = async (url = api) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    userList = data.results;
+    dogList = data.message;
 
-    display(userList);
+    display(dogList);
   } catch (error) {
     console.log(error);
   }
 };
 
-fetchUsers();
+fetchDogs();
 
-const display = () => {
+const display = (dogList) => {
   let dogCard = "";
   console.log(dogList);
+
+  dogList.forEach((dog) => {
+    dogCard += `<div class="card" style="width: 18rem">
+    <img src="${dog}" class="card-img-top" alt="Dog Image" />
+    <div class="card-body">
+      <h5 class="card-title">Name</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </p>
+    </div>
+  </div> `;
+  });
+  dogListELm.innerHTML = dogCard;
+  document.getElementById("dogCount").innerText = dogList.length;
 };
